d1372bad76e9a10f09962bd1685e950ff648f3aa
Matthew Ko <matt.ck.ko@gmail.com>
2018-07-30 23:42:47 -0700

    More changes

---
+++ NhlApp/StatsApiUnitTests/StatTeamApiTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace StatsApiUnitTests
+{
+    public class StatTeamApiTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //var result = StatsApiAdapter.GetAllTeams();
+
+            Assert.False(result, "1 should not be prime");
+        }
+    }
+}
